c8aa3799d815391c24e809a05143a140f0bc091f
André Oliveira <andre.zacarias@ufn.edu.br>
2022-05-12 22:10:20 -0300

    Exercicios de 12/05/2022

---
+++ Back-End/Maio/12052022/Exercicio_2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Faça um programa em VS que solicite um número inteiro positivo ao usuário,
+// validando a entrada de dados 
+// (informando se ele estiver errado e repetindo a solicitação até que esteja correto). 
+// Após o programa em VS deve informar todos os números pares existentes entre 1 e o
+// número fornecido pelo usuário.
+
+// Exemplo:
+// Digite um número inteiro positivo: -8
+// Valor incorreto!
+// Digite um número inteiro positivo: 8
+// Numero digitado: 8
+// Números inteiros pares entre 1 e 8: 2, 4, 6.
+
+
+
+
+
+int numeroInteiro;
+int par = 0;
+
+do
+{
+
+    Console.Write("Digite um número inteiro entre 1 e 50:");
+    numeroInteiro = int.Parse(Console.ReadLine());
+    if (numeroInteiro > 0 && numeroInteiro <= 50)
+    {
+        Console.WriteLine("Número digitado: " + numeroInteiro);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Valor deve ser entre 1 e 50!");
+
+    }
+
+} while (true);
+
+while (par <= numeroInteiro - 2)
+{
+
+    Console.Write((par = par + 2) + ",");
+
+}
---
+++ Back-End/Maio/Exemplo_repeticao/Program.cs
@@ -0,0 +1,70 @@
+﻿// Repetição para validar entrada em variáveis
+
+//float notaBimestral;
+
+//do
+//{
+//    Console.Clear();
+//    Console.WriteLine("Digite uma nota válida [0 a 10]: ");
+//    notaBimestral = float.Parse(Console.ReadLine());
+
+//    if (notaBimestral < 0 || notaBimestral > 10)
+//    {
+//        Console.WriteLine("Nota Inválida!!!");
+//        Console.ReadKey();
+
+//    }
+
+//} while(notaBimestral < 0 || notaBimestral > 10);    
+
+//List<string> listaPessoas = new List<string>();
+//string nome;
+
+//do
+//{
+//    Console.WriteLine("Digite um nome de pessoa ou a palavra SAIR para encerrar o programa: ");
+//    nome = Console.ReadLine().ToUpper();
+
+//    if(nome == "SAIR")
+//    {
+//        break; //encerra a repetição
+//    }
+//    listaPessoas.Add(nome);
+
+//}while(true);
+
+////listaPessoas = [ 0, 1, 2, 3, ..,n]
+//for(int i = 0; i < listaPessoas.Count; i++)
+//{
+//    Console.WriteLine(listaPessoas[i]);
+//}
+
+int valorGlicemia; //45 a 450
+List<int> listaGlicemias = new List<int>();
+
+int TOTAL = 5;
+for (int i = 0; i < TOTAL; i++)
+{
+
+    do
+    {
+        Console.Write("Digite um valor de glicemia válido [45 a 450]: ");
+        valorGlicemia = int.Parse(Console.ReadLine());
+        if (valorGlicemia >= 45 && valorGlicemia <= 450)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Valor inválido. Atente para 45 a 450!");
+        }
+    } while (true);
+
+    listaGlicemias.Add(valorGlicemia);
+
+}
+
+for (int i = 0; i < listaGlicemias.Count; i = i + 1)
+{
+    Console.WriteLine(listaGlicemias[i]);
+}
