c844b3305448b166ef6ce816f54b6147c1ad61bc
André Oliveira <andre.zacarias@ufn.edu.br>
2022-05-23 19:52:39 -0300

    Atualização 23052022

---
+++ Back-End/Maio/20052022/AulaOO/Carro_exercicio/Program.cs
@@ -0,0 +1,51 @@
+﻿ // Crie uma classe Carro com os atributos: modelo, marca, cor e placa. Crie um método para definir os
+ // atributos, outro método para ligar o carro e outro método para desligar o carro.
+
+Carro c = new Carro();
+
+Console.WriteLine("Entre com a marca,modelo,cor e a placa do carro.");
+c.marca = Console.ReadLine();
+c.modelo = Console.ReadLine();
+c.cor = Console.ReadLine();
+c.placa = Console.ReadLine();
+
+Console.WriteLine("Digite + para ligado ou - para desligado.");
+c.onoff = Console.ReadLine();
+
+if(c.onoff == "+")
+{
+    c.ligar();
+}
+else
+{
+    c.desligar();
+}
+
+
+Console.WriteLine($"O carro modelo {c.modelo} da marca {c.marca} cor {c.cor} e placa {c.placa} está {c.onoff} ");
+
+public class Carro
+{
+    public string modelo;
+    public string marca;
+    public string cor;
+    public string placa;
+    public string onoff;
+
+    public bool ligado;
+
+    public void ligar()
+    {
+        
+      onoff = "ligado";            
+    
+    }
+
+    public void desligar()
+    {
+
+        onoff = "desligado";
+
+    }
+
+}
---
+++ Back-End/Maio/20052022/AulaOO/Projeto_OO/Program.cs
@@ -0,0 +1,53 @@
+﻿Lampada l = new Lampada();
+l.potencia = 9;
+l.ligar();
+Console.WriteLine("Ligada? = " +l.ligado + " / Potencia = " + l.potencia);
+l.desligar();
+Console.WriteLine("Ligada? = " + l.ligado + " / Potencia = " + l.potencia);
+double n;
+n = l.retornaPotencia;
+Console.WriteLine("A potência da lampada cadastrada = " + n);
+Console.WriteLine("Retorno da função estaLigado = " + l.estaLigado());
+l.ligar();
+l.desligar();
+
+if (l.estaLigada())
+{
+    Console.WriteLine("A lampada está ligada!");
+}
+else 
+{
+    Console.WriteLine("A lampada está desligada!");
+}
+
+public class Lampada
+{
+    public bool ligado; //aqui é true ou false! - Atributo
+    public double potencia; // em OFF
+    //métodos
+
+    // visibilidade (public, private ou protected)
+    // tipo de retorno -> void - não retorna nada
+    // tipo de retorno -> int - retorna int
+
+    public void ligar() 
+    {
+        Console.WriteLine("A lampada está sendo ligada!");
+        ligado = true;
+    }
+    public void desligar () 
+    {
+        Console.WriteLine("A lâmpada está sendo desligada!");
+        ligado = false;
+    }
+
+    public double retornaPotencia() 
+    { 
+        return potencia;
+    }    
+    public bool estaLigada()
+    {
+        return ligado; 
+    }
+}
+
