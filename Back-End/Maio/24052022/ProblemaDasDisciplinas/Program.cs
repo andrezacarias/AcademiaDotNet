14afaa77220797efdeefb614f8513b2d1e16ff63
André Oliveira <andre.zacarias@ufn.edu.br>
2022-05-25 19:13:01 -0300

    Atualização pre aula 25052022

---
+++ Back-End/Maio/24052022/ProblemaDasDisciplinas/Program.cs
@@ -0,0 +1,10 @@
+﻿// Orientação a Objetos -> TERCEIRIZAÇÃO
+
+//        - Problema das listagens dos alunos (disciplinas de Estruturas de Dados e Linguagem de Programação)
+//        -Arquivos.txt com nomes dos matriculados (matricula; nome; email)
+
+//        Listas->List(Count, Add(), AddRange(), Remove(), Contains())
+//        Console->Console(WriteLine(), ReadLine(), ReadKey(), Clear())
+//        Arquivo Texto -> StreamReader (ReadLine(), StreamReader(), EndOfStream(), Close())
+//        Aluno->criar a classe aluno -> seus atributos e seus serviços
+//        Utilidades -> criar a classe Util -> com serviços comuns de arquivo e listas
