0de25660136b18861fdd0015b6a62c3b277e8f62
André Oliveira <andre.zacarias@ufn.edu.br>
2022-05-26 18:07:24 -0300

    Atualizacao pre aula 26052022

---
+++ Back-End/Maio/25052022/OO_AnimalDeEstimacao/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Crie um programa em C# no qual o usuário deverá informar o nome e o tipo de cinco animais de estimação. 
+O programa deverá exibir na tela para o usuário quantos Cachorros, Gatos e Peixes foram informados.
+
+Regras que deverão ser seguidas para a implementação do programa:
+
+Os únicos tipos de animais válidos são Cachorro, Gato, Peixe.
+Caso seja informado um tipo diferente o programa repetir até o usuário digitar um tipo válido.
+É obrigatório criar uma classe para representar o Animal.
+A classe deverá possuir dois dados privados (propriedades) para representar as características do animal.
+A classe deverá possuir métodos de acesso (propriedade getter e setter) para permitir que o usuário 
+armazene/leia os dados dos dois dados privados (propriedades).*/
+
+
+
+using OO_AnimalDeEstimacao;
+
+Console.WriteLine("Vou apresentar meus bichos de estimação!");
+String nome="", especie="";
+int contadorGato=0, contadorCachorro=0, contadorPeixe=0;
+for (int i = 0; i < 6; i++)
+{
+   
+    Console.WriteLine("Digite o nome do bicho: ");
+nome = Console.ReadLine();
+Console.WriteLine("Digite a espécie do bicho: ");
+especie = Console.ReadLine().ToLower();
+    
+
+if (especie == "gato" || especie == "cachorro" || especie == "peixe")
+    {
+        if (especie == "gato")
+        {
+            contadorGato++;
+        }
+        else if(especie == "cachorro")
+        {
+            contadorCachorro++;
+        }
+        else if (especie == "peixe")
+        {
+            contadorPeixe++;
+
+        }
+        Console.Clear();
+    }
+    else
+    {
+        Console.WriteLine("Digite uma espécie válida (cachorro/gato/peixe).");
+        i--;
+    }
+
+
+}
+
+
+    BichoDeEstimacao m = new BichoDeEstimacao(nome, especie , contadorGato, contadorCachorro, contadorPeixe );
+m.mostraAnimalDeEstimacao();
+
---
+++ Back-End/Maio/25052022/OO_AnimalDeEstimacao/animalDeEstimacao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OO_AnimalDeEstimacao
+{
+    public class BichoDeEstimacao
+    {
+        //atributos
+        private String nome;
+        private String especie;
+        public int contadorGato;
+        public int contadorCachorro;
+        public int contadorPeixe;
+
+        //construtor
+        public BichoDeEstimacao(string nome, string especie, int contadorGato, int contadorCachorro, int contadorPeixe)
+        {
+            this.nome = nome;
+            this.especie = especie;
+            this.contadorGato = contadorGato;
+            this.contadorCachorro = contadorCachorro;
+            this.contadorPeixe = contadorPeixe;
+        }
+
+        //metodos
+        public void mostraAnimalDeEstimacao()
+        {
+            Console.WriteLine($"Eu tenho {contadorGato} gatos ,{contadorCachorro} cachorros e {contadorPeixe} peixes.");
+        }
+
+    }
+}
