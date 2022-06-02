129a42a0c3f9a258d0233f34457a6e0027c25559
André Oliveira <andre.zacarias@ufn.edu.br>
2022-06-01 22:03:44 -0300

    Aula 01062022

---
+++ Back-End/Junho/WindowsForm/01062022/WindowsForm01/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsForm01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTOk = new System.Windows.Forms.Button();
+            this.BTCancel = new System.Windows.Forms.Button();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.tbEndereco = new System.Windows.Forms.TextBox();
+            this.tbTelefone = new System.Windows.Forms.TextBox();
+            this.tbBairro = new System.Windows.Forms.TextBox();
+            this.tbCidade = new System.Windows.Forms.TextBox();
+            this.tbSexo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BTOk
+            // 
+            this.BTOk.Location = new System.Drawing.Point(13, 255);
+            this.BTOk.Name = "BTOk";
+            this.BTOk.Size = new System.Drawing.Size(179, 72);
+            this.BTOk.TabIndex = 0;
+            this.BTOk.Text = "OK";
+            this.BTOk.UseVisualStyleBackColor = true;
+            this.BTOk.Click += new System.EventHandler(this.BTOk_Click);
+            // 
+            // BTCancel
+            // 
+            this.BTCancel.Location = new System.Drawing.Point(246, 255);
+            this.BTCancel.Name = "BTCancel";
+            this.BTCancel.Size = new System.Drawing.Size(179, 72);
+            this.BTCancel.TabIndex = 1;
+            this.BTCancel.Text = "Cancelar";
+            this.BTCancel.UseVisualStyleBackColor = true;
+            this.BTCancel.Click += new System.EventHandler(this.BTCancel_Click);
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(74, 12);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(350, 23);
+            this.tbNome.TabIndex = 2;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(25, 20);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(43, 15);
+            this.lblNome.TabIndex = 3;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(25, 58);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(39, 15);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "Email:";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(74, 50);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(350, 23);
+            this.tbEmail.TabIndex = 5;
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Location = new System.Drawing.Point(8, 99);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(59, 15);
+            this.lblEndereco.TabIndex = 6;
+            this.lblEndereco.Text = "Endereço:";
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(13, 130);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(54, 15);
+            this.lblTelefone.TabIndex = 7;
+            this.lblTelefone.Text = "Telefone:";
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Location = new System.Drawing.Point(23, 159);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(41, 15);
+            this.lblBairro.TabIndex = 8;
+            this.lblBairro.Text = "Bairro:";
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Location = new System.Drawing.Point(17, 189);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(47, 15);
+            this.lblCidade.TabIndex = 9;
+            this.lblCidade.Text = "Cidade:";
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Location = new System.Drawing.Point(25, 216);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(35, 15);
+            this.lblSexo.TabIndex = 10;
+            this.lblSexo.Text = "Sexo:";
+            // 
+            // tbEndereco
+            // 
+            this.tbEndereco.Location = new System.Drawing.Point(74, 91);
+            this.tbEndereco.Name = "tbEndereco";
+            this.tbEndereco.Size = new System.Drawing.Size(350, 23);
+            this.tbEndereco.TabIndex = 11;
+            // 
+            // tbTelefone
+            // 
+            this.tbTelefone.Location = new System.Drawing.Point(74, 122);
+            this.tbTelefone.Name = "tbTelefone";
+            this.tbTelefone.Size = new System.Drawing.Size(351, 23);
+            this.tbTelefone.TabIndex = 12;
+            // 
+            // tbBairro
+            // 
+            this.tbBairro.Location = new System.Drawing.Point(74, 151);
+            this.tbBairro.Name = "tbBairro";
+            this.tbBairro.Size = new System.Drawing.Size(350, 23);
+            this.tbBairro.TabIndex = 13;
+            // 
+            // tbCidade
+            // 
+            this.tbCidade.Location = new System.Drawing.Point(76, 184);
+            this.tbCidade.Name = "tbCidade";
+            this.tbCidade.Size = new System.Drawing.Size(349, 23);
+            this.tbCidade.TabIndex = 14;
+            // 
+            // tbSexo
+            // 
+            this.tbSexo.Location = new System.Drawing.Point(76, 216);
+            this.tbSexo.Name = "tbSexo";
+            this.tbSexo.Size = new System.Drawing.Size(349, 23);
+            this.tbSexo.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 354);
+            this.Controls.Add(this.tbSexo);
+            this.Controls.Add(this.tbCidade);
+            this.Controls.Add(this.tbBairro);
+            this.Controls.Add(this.tbTelefone);
+            this.Controls.Add(this.tbEndereco);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.lblBairro);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.BTCancel);
+            this.Controls.Add(this.BTOk);
+            this.Name = "Form1";
+            this.Text = "Formulário da primeira aplicação";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button BTOk;
+        private Button BTCancel;
+        private TextBox tbNome;
+        private Label lblNome;
+        private Label lblEmail;
+        private TextBox tbEmail;
+        private Label lblEndereco;
+        private Label lblTelefone;
+        private Label lblBairro;
+        private Label lblCidade;
+        private Label lblSexo;
+        private TextBox tbEndereco;
+        private TextBox tbTelefone;
+        private TextBox tbBairro;
+        private TextBox tbCidade;
+        private TextBox tbSexo;
+    }
+}
---
+++ Back-End/Junho/WindowsForm/01062022/WindowsForm01/Form1.cs
@@ -0,0 +1,30 @@
+namespace WindowsForm01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTOk_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Cliquei no botão OK!");
+            MessageBox.Show("O nome do usuário digitado é " + tbNome.Text + "\n" +
+                "  o e-mail do usuário digitado é " + tbEmail.Text + "\n" +
+                "  o endereço do usuário digitado é " + tbEndereco.Text + "\n" +
+                "  o telefone do usuário digitado é " + tbTelefone.Text + "\n" +
+                "  o bairro do usuário digitado é " + tbBairro.Text + "\n" +
+                "  a cidade do usuário digitado é " + tbCidade.Text + "\n" +
+                "  o sexo do usuário digitado é " + tbSexo.Text);
+            MessageBox.Show("Cadastro efetuado com sucesso!");
+        }
+
+        private void BTCancel_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cliquei no botão Cancelar!");
+        }
+
+   
+    }
+}
